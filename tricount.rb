d5857da0162105304de066286884a9f2ee96f0ea
cardon <philantrope1@outlook.com>
2024-09-19 20:26:24 +0200

    cours algo

---
+++ tricount.rb
@@ -0,0 +1,36 @@
+class Tricount
+  def add_user(name)
+
+  end
+
+  def add_bill(player,total,recipients)
+
+  end
+
+  def balance
+
+  end
+end
+
+
+tri_count = Tricount.new
+
+tri_count.add_user()
+tri_count.add_user()
+tri_count.add_user()
+tri_count.add_user()
+tri_count.add_user()
+tri_count.add_user()
+
+tri_count.add_bill()
+tri_count.add_bill()
+tri_count.add_bill()
+tri_count.add_bill()
+tri_count.add_bill()
+tri_count.add_bill()
+tri_count.add_bill()
+tri_count.add_bill()
+tri_count.add_bill()
+tri_count.add_bill()
+
+tri_count.balance
